0a1a5a4afadefe32b1658072900f6cf04c940c25
Mk Iskayyy <markkristophern@gmail.com>
2024-01-25 08:39:44 +0400

    Added pages, layout, error, loading and not found

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+const links = [
+  { title: "Homepage", path: "/" },
+  { title: "About", path: "/about" },
+  { title: "Contact", path: "/contact" },
+  { title: "Blog", path: "/blog" },
+];
+
+const Links = () => {
+  return (
+    <div>
+      {links.map((link) => {
+        return (
+          <Link key={link.title} href={link.path}>
+            {link.title}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Links;
